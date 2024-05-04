9eb610ca7cef067c98ec2a0a75f104e7f9cd394a
Decodeine <abionaadesola@yahoo.com>
2024-05-04 01:44:32 +0100

    add swagger documentation using flask-restx

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_restx import Api, Resource, reqparse
 from model import db, Book
 from Schema import BookSchema
 import os
@@ -12,54 +13,71 @@ db.init_app(app)
 
 book_schema = BookSchema()
 
-with app.app_context():
-    db.create_all()
-@app.route('/books', methods=['POST'])
-def create_book():
-    errors = book_schema.validate(request.json)
-    if errors:
-        return jsonify(errors), 400
-    new_book = Book(**request.json)
-    db.session.add(new_book)
-    db.session.commit()
-    return book_schema.dump(new_book), 201
+api = Api(app, version='1.0', title='Book API', description='A simple crud operation Book API')
 
-@app.route('/books', methods=['GET'])
-def get_books():
-    all_books = Book.query.all()
-    return jsonify([{'id': book.id, 'title': book.title, 'author': book.author, 'year': book.year, 'isbn': book.isbn} for book in all_books]), 200
+ns = api.namespace('books', description='Book operations')
+#  a parser for the book data
+book_parser = reqparse.RequestParser()
+book_parser.add_argument('title', type=str, required=True, help='Title cannot be blank!')
+book_parser.add_argument('author', type=str, required=True, help='Author cannot be blank!')
 
-@app.route('/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    book = Book.query.get(book_id)
-    if book:
-        return jsonify({'id': book.id, 'title': book.title, 'author': book.author, 'year': book.year, 'isbn': book.isbn}), 200
-    else:
-        return jsonify({'error': 'Book not found'}), 404
+@ns.route('/')
+class BookList(Resource):
 
-@app.route('/books/<int:book_id>', methods=['PUT'])
-def update_book(book_id):
-    book = Book.query.get(book_id)
-    if book:
-        errors = book_schema.validate(request.json, partial=True) 
-        if errors:
-            return jsonify(errors), 400
-        for key, value in request.json.items():
-            setattr(book, key, value)
-        db.session.commit()
-        return book_schema.dump(book), 200
-    else:
-        return jsonify({'error': 'Book not found'}), 404
+    def get(self):
+        #List all books
+        all_books = Book.query.all()
+        result = book_schema.dump(all_books, many=True)
+        return jsonify(result),200
+
+    @ns.expect(book_parser)
+    def post(self):
+    #Create a new book
+       data = book_parser.parse_args()
+       errors = book_schema.validate(data)
+       if errors:
+          return errors, 400
+
+       new_book = Book(title=data['title'], author=data['author'])
+       db.session.add(new_book)
+       db.session.commit()
+       return book_schema.dump(new_book), 201
     
-@app.route('/books/<int:book_id>', methods=['DELETE'])
-def delete_book(book_id):
-    book = Book.query.get(book_id)
-    if book:
-        db.session.delete(book)
-        db.session.commit()
-        return jsonify({'status': 'Book deleted'}), 200
-    else:
-        return jsonify({'error': 'Book not found'}), 404
+@ns.route('/<int:book_id>')
+class Book(Resource):
+    
+    def get(self, book_id):
+        book = Book.query.get(book_id)
+        if book:
+            return jsonify({'id': book.id, 'title': book.title, 'author': book.author, 'year': book.year, 'isbn': book.isbn}), 200
+        else:
+            return jsonify({'error': 'Book not found'}), 404
+
+    @ns.expect(book_parser) 
+    def put(self, book_id):
+        book = Book.query.get(book_id)
+        if book:
+            data = book_parser.parse_args()
+            errors = book_schema.validate(data, partial=True) 
+            if errors:
+                return jsonify(errors), 400
+            for key, value in data.items():
+                setattr(book, key, value)
+            db.session.commit()
+            return book_schema.dump(book), 200
+        else:
+            return jsonify({'error': 'Book not found'}), 404
+        
+    def delete(self, book_id):
+        book = Book.query.get(book_id)
+        if book:
+            db.session.delete(book)
+            db.session.commit()
+            return jsonify({'status': 'Book deleted'}), 200
+        else:
+            return jsonify({'error': 'Book not found'}), 404
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
